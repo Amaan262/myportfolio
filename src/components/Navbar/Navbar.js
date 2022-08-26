cfec1ac499c5bd3b5fdf8e1d884b8e07d173fb24
Amaan Ahmad <Amaansaifi262@gmail.com>
2022-08-26 22:22:00 +0530

    optimizing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
-// import logo from "./logoWhite.png";
 function Navbar() {
   return (
     <nav id="navbar">
       <div className="logo">
         <a href={process.env.PUBLIC_URL}>
-          <img src={process.env.PUBLIC_URL+"/images/img/logoWhite.png"} alt="Amaan Ahmad" />
+          <img
+            src={process.env.PUBLIC_URL + "/images/img/logoWhite.png"}
+            alt="Amaan Ahmad"
+          />
         </a>
       </div>
       <ul>
@@ -15,7 +17,7 @@ function Navbar() {
           </a>
         </li>
         <li>
-          <a href="#web-design" >Web Design</a>
+          <a href="#web-design">Web Design</a>
         </li>
         <li>
           <a href="#contact">Contact</a>
